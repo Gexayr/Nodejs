aa0e238f54f1f9e6c04f428534d711ae9637d523
gexayr <gexo7777@gmail.com>
2022-08-14 15:24:53 +0400

    create package.json

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const PORT = 4000;
+const server = http.createServer((req,res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!'
+    }));
+});
+
+console.info(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
